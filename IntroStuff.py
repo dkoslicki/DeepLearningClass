d62a604eb9c67e8d35feb0e241f195e5a28947d6
dkoslicki <david.koslicki@math.oregonstate.edu>
2018-08-10 16:32:13 -0700

    More intro stuff

--- IntroStuff.py
+++ IntroStuff.py
@@ -2,7 +2,7 @@
 
 # Workshop examples
 import numpy as np
-
+import pandas as pd
 # vectors
 a = np.array([1, 2, 3, 4], float)
 print(a)
@@ -22,3 +22,36 @@ a[0, 1]
 a[:, 1]
 
 # np.zeros, np.ones, etc.
+
+# Pandas!
+
+data = np.array([['', 'A', 'B', 'C'],
+                 ['Row0', 1, 2, 3],
+                 ['Row1', 3, 4, 5],
+                 ['Row2', 6, 7, 8]])
+
+
+df = pd.DataFrame(data=data[1:, 1:],
+                  index=data[1:, 0],
+                  columns=data[0, 1:])
+
+# alternatively, to create row-wise
+cols = {"A": [1, 3, 6],
+		"B": [2, 4, 7],
+		"C": [3, 5, 8]}
+
+df = pd.DataFrame(data=cols)
+
+# grabbing values
+print(df.iloc[0][0])
+
+# another way to access stuff
+df.loc[:, 'B']
+df.loc[:, 'C']
+
+df['A']
+df.to_csv('test.csv')
+
+df2 = pd.read_csv('test.csv')
+
+
